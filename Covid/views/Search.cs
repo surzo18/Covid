4ab05b442800a0008e2cdba892d51fa33c64bdb9
Adrian Mindek <adrian.mindek@gmail.com>
2021-01-17 19:16:57 +0100

    Auto stash before merge of "main" and "origin/main"

--- Covid/views/Search.cs
+++ Covid/views/Search.cs
@@ -28,58 +28,24 @@ namespace Covid
        
         private void Search_Load(object sender, EventArgs e)
         {
-            /*
-            List<List<string>> databaza = new List<List<string>>();
-            List<string> user1 = new List<string>();
-            user1.Add("10."); 
-            user1.Add("Jožko Mrkvička");
-            user1.Add("SPŠ IT");
-            user1.Add("561235/7842");
-            user1.Add("III.AI");
-            databaza.Add(user1);
-            List<string> user2 = new List<string>();
-            user2.Add("45.");
-            user2.Add("Lukaš Rezetka");
-            user2.Add("SPŠ IT");
-            user2.Add("458732/4823");
-            user2.Add("IV.BI");
-            databaza.Add(user2);
-            List<string> user3 = new List<string>();
-            user3.Add("13.");
-            user3.Add("Martin Haranta");
-            user3.Add("SOUKNM");
-            user3.Add("784535/7865");
-            user3.Add("I.A");
-            databaza.Add(user3);
-            List<string> user4 = new List<string>();
-            user4.Add("99.");
-            user4.Add("Marek Horvát");
-            user4.Add("SPŠ IT");
-            user4.Add("845694/7834");
-            user4.Add("I.CI");
-            databaza.Add(user4);
-            List<string> user5 = new List<string>();
-            user5.Add("99.");
-            user5.Add("Ján Štens");
-            user5.Add("SPŠ IT");
-            user5.Add("236578/7896");
-            user5.Add("III.AG");
-            databaza.Add(user5);
-
-            guna2DataGridView1.Rows.Add(databaza.Count);
-            for(int i=0;i<databaza.Count;i++)
-            {
-                int j = 0;
-                while(j<(databaza[0].Count))
-                {
-                    guna2DataGridView1.Rows[i].Cells[j].Value = databaza[i][j];
-                    j++;
-                }
-                guna2DataGridView1.Rows[i].Cells[j].Value = "Pridaj";
-            }
-            */
+            this.refereshDatagrid();
         }
 
+
+        private void refereshDatagrid()
+        {
+            if (this.personResults.Count <= 0) return;
+            guna2DataGridView1.Rows.Add(this.personResults.Count);
+            for (int i = 0; i < this.personResults.Count; i++)
+            {
+                guna2DataGridView1.Rows[i].Cells[0].Value = this.personResults[i].id;
+                guna2DataGridView1.Rows[i].Cells[1].Value = this.personResults[i].surname + " " + this.personResults[i].name;
+                guna2DataGridView1.Rows[i].Cells[2].Value = this.personResults[i].company.name;
+                guna2DataGridView1.Rows[i].Cells[3].Value = this.personResults[i].id_number;
+                guna2DataGridView1.Rows[i].Cells[4].Value = this.personResults[i].study_year + "." + this.personResults[i].year_letter;
+                guna2DataGridView1.Rows[i].Cells[5].Value = "Pridaj";
+            }
+        }
 
         private void parseInputFromSearch(string input)
         {
@@ -120,7 +86,7 @@ namespace Covid
                     int age = Convert.ToInt32(rdr["Age"]);
                     string birth_date = rdr["Birth_date"].ToString();
                     string year_letter = rdr["Year_letter"].ToString();
-                    this.personResults.Append(new Person(
+                    this.personResults.Add(new Person(
                         id,schoold_id,role_id,name,surname,study_year,id_number,address,phone,email,age,birth_date,year_letter
                         ));
                 }
@@ -134,8 +100,10 @@ namespace Covid
             //nastavi hladane na aktualnu hodnotu
             this.parseInputFromSearch(guna2TextBox1.Text);
 
-            //Clearne grid vysledkov
+            //Clearne grid vysledkov 
             guna2DataGridView1.Rows.Clear();
+            this.personResults = new List<Person>();
+          
 
             //ak je priezvisko prazdne nehladaj nič
             if(this.searchedSurname == "")
@@ -145,11 +113,17 @@ namespace Covid
 
             //Loadne nove vyhľadávané data
             this.loadDataForGridFromDb();
-            //
+            //Refreshne datagrid
+            this.refereshDatagrid();
+
+            this.searchedName = "";
+            this.searchedSurname = "";
         }
 
         private void guna2DataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
+            Person clickedOne = this.personResults[e.RowIndex];
+            MessageBox.Show(clickedOne.surname);
             
         }
       
